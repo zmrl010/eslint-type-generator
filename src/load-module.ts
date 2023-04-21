d25793813fa63c0018f8e956873d3cd7f6fda8a4
Zach <zmriel@gmail.com>
2023-04-21 13:51:31 -0400

    refactor: format text via context

--- src/load-module.ts
+++ src/load-module.ts
@@ -2,7 +2,7 @@
  * Dynamically load a module
  *
  * @param name name of module to import
- * @returns default export if exists, else module
+ * @returns default export if exists, else the entire module
  */
 export async function loadModule(name: string): Promise<unknown> {
   const module = await import(name);
